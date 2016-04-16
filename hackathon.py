0d9b9d8550d4fab7afa7fec7daa93bdd4f8eec8c
Sean Winters <sean.m.winters@gmail.com>
2016-04-16 04:53:22 -0700

    New changes

--- hackathon.py
+++ hackathon.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+admin = Admin(app, name='pingins', template_mode='bootstrap3')
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.TEXT)
     balance = db.Column(db.REAL)
+    parking = db.Column(db.BOOLEAN)
+    checkin = db.Column(db.BOOLEAN)
+    security = db.Column(db.BOOLEAN)
+    shopping = db.Column(db.BOOLEAN)
+    departure = db.Column(db.BOOLEAN)
 
 db.create_all()
 
+admin.add_view(ModelView(User, db.session))
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 api_manager.create_api(User, methods=['GET', 'POST', 'DELETE', 'PUT'])
 
